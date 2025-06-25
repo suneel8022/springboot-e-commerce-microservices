79211a70cb6e8147838d9dbfded786c6f744c318
Suneel <suneelhere8022@gmail.com>
2025-06-25 22:57:07 +0530

    Setting up product-service initial setup along with config

---
+++ services/product-service/src/test/java/com/suneel/ecommerce/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suneel.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
